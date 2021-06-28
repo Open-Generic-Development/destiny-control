e73048d88f385db1f4c3dbd99e3cd2c6767fccc1
Christian B <37768199+Bloeckchengrafik@users.noreply.github.com>
2021-06-28 18:52:11 +0000

    Initial Commit

---
+++ src/main/java/systems/ogd/destinycontrol/Destiny.java
@@ -0,0 +1,17 @@
+package systems.ogd.destinycontrol;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Destiny extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
